6868cd5f2c7d40449722c50b9a1201cf0917ee02
mohamed <elouariachimohamed@gmail.com>
2026-02-21 00:39:31 +0100

    añadido poder descargar cualquier link

--- routes/pruebas.js
+++ routes/pruebas.js
@@ -4,47 +4,54 @@ var path = require('path');
 var router = express.Router();
 
 // 1. Función asíncrona para descargar el video
-async function descarga() {
+async function descarga(url) {
     console.log('Iniciando descarga con yt-dlp...');
-    const url = 'https://youtu.be/NvNKO31H8_M?si=YF8cz5h-W9fdIgJK';
+    const info = await ytDlp(url, { dumpSingleJson: true });
+    const title = info.title
+    const ext = 'mp3'
+    const nombreArchivo = `${title}.${ext}`;
 
     // Definimos la ruta exacta donde se guardará el archivo en tu servidor
-    const rutaDestino = path.join(__dirname, '../source/mi_video.mp4');
+    var rutaDestino = path.join(__dirname, '../source');
+    rutaDestino = path.join(rutaDestino, nombreArchivo);
 
     // Retornamos la promesa para que el servidor pueda "esperar" a que termine
-    return ytDlp(url, {
-        output: rutaDestino, // Le decimos exactamente dónde guardarlo
+    await ytDlp(url, {
+        output: rutaDestino,
         format: 'bestaudio/best[height<=720]',
         mergeOutputFormat: 'mp4'
-    }).then(() => {
-        console.log('¡Descarga finalizada en el servidor!');
-    });
+    })
+    return [rutaDestino, nombreArchivo]
 }
 
 // 2. RUTA POST: El cliente la llama para iniciar la descarga del video
 router.post('/descargar', async function(req, res, next) {
     try {
         // Esperamos a que la función de yt-dlp termine de descargar el video
-        await descarga();
+        const [path, name] = await descarga(req.body.link);
+        console.log(path);
 
         // Una vez que terminó, le respondemos al HTML con un JSON de éxito
         res.json({
             mensaje: "El video se descargó en el servidor correctamente.",
-            estado: "completado"
+            estado: "completado",
+            path: path,
+            name: name
         });
     } catch (error) {
         console.error('Error al descargar:', error);
-        res.status(500).json({ mensaje: "Hubo un error al descargar el video." });
+        res.status(500).json({ mensaje: "Hubo un error al descargar el video.", path: path});
     }
 });
 
 // 3. RUTA GET: El cliente es redirigido aquí para llevarse el archivo físico
 router.get('/obtener-archivo', function(req, res, next) {
     // Buscamos el video que acabamos de descargar
-    const directorio = path.join(__dirname, '../source/mi_video.mp4');
+    const directorio = req.query.path;
+    const name = req.query.name;
 
     // Se lo enviamos al usuario
-    res.download(directorio, 'video_youtube.mp4', function(error) {
+    res.download(directorio, name, function(error) {
         if (error) {
             console.log("Hubo un error al enviar el archivo al navegador:", error);
             if (!res.headersSent) {
