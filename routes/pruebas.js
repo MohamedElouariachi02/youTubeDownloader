03150a9b5f1dffaf2a06d543415ef7b9919aabeb
mohamed <elouariachimohamed@gmail.com>
2026-02-21 18:11:45 +0100

    añadido seleccion de formatos (no funcional) y manejo de errores

--- routes/pruebas.js
+++ routes/pruebas.js
@@ -5,26 +5,33 @@ var router = express.Router();
 
 // 1. Función asíncrona para descargar el video
 async function descarga(url) {
-    console.log('Iniciando descarga con yt-dlp...');
-    const rutaCookie = path.join(__dirname, '../cookie.txt');
-    const info = await ytDlp(url, { dumpSingleJson: true, cookies: rutaCookie, });
-    const title = info.title.replace(/[^a-zA-Z0-9 _-]/g, "");
-    const ext = 'mp4'
-    const nombreArchivo = `${title}.${ext}`;
+    try {
+        console.log('Iniciando descarga con yt-dlp...');
+        const rutaCookie = path.join(__dirname, '../cookie.txt');
+        const info = await ytDlp(url, { dumpSingleJson: true, cookies: rutaCookie, });
+        const title = info.title.replace(/[^a-zA-Z0-9 _-]/g, "");
+        const ext = 'mp4'
+        const nombreArchivo = `${title}.${ext}`;
 
-    // Definimos la ruta exacta donde se guardará el archivo en tu servidor
-    var rutaDestino = path.join(__dirname, '../source');
-    rutaDestino = path.join(rutaDestino, nombreArchivo);
+        // Definimos la ruta exacta donde se guardará el archivo en tu servidor
+        var rutaDestino = path.join(__dirname, '../source');
+        rutaDestino = path.join(rutaDestino, nombreArchivo);
 
 
-    // Retornamos la promesa para que el servidor pueda "esperar" a que termine
-    await ytDlp(url, {
-        output: rutaDestino,
-        format: 'bestaudio/best[height<=720]',
-        cookies: rutaCookie,
-        mergeOutputFormat: 'mp4'
-    })
-    return [rutaDestino, nombreArchivo]
+        // Retornamos la promesa para que el servidor pueda "esperar" a que termine
+        await ytDlp(url, {
+            output: rutaDestino,
+            format: 'bestaudio/best[height<=720]',
+            cookies: rutaCookie,
+            mergeOutputFormat: 'mp4'
+        })
+        return [rutaDestino, nombreArchivo]
+    }
+    catch (error) {
+        console.error("Url no valida");
+        return [undefined, undefined]
+    }
+
 }
 
 // 2. RUTA POST: El cliente la llama para iniciar la descarga del video
@@ -32,8 +39,16 @@ router.post('/descargar', async function(req, res, next) {
     try {
         // Esperamos a que la función de yt-dlp termine de descargar el video
         const [path, name] = await descarga(req.body.link);
-        console.log(path);
-
+        if (!path || !name)
+        {
+            res.json({
+                mensaje: "Url no valida",
+                estado: "incompleto",
+                path: path,
+                name: name
+            });
+            return
+        }
         // Una vez que terminó, le respondemos al HTML con un JSON de éxito
         res.json({
             mensaje: "El video se descargó en el servidor correctamente.",
@@ -52,6 +67,11 @@ router.get('/obtener-archivo', function(req, res, next) {
     // Buscamos el video que acabamos de descargar
     const directorio = req.query.path;
     const name = req.query.name;
+    if (!directorio || !name)
+    {
+        console.log("Holaaaa ", error);
+        return
+    }
 
     // Se lo enviamos al usuario
     res.download(directorio, name, function(error) {
